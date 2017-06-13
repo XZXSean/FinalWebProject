b485c72784a88c937fec5d4187ee333dca852aff
1074332133 <1074332133@qq.com>
2017-06-13 20:59:29 +0800

    修改后的列表项目1

--- web/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k,i,[_(c,l,e,f,g,m,i,[_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s)]),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E)])])]);}; 
-var b="rootNodes",c="pageName",d="登录界面",e="type",f="Wireframe",g="url",h="登录界面.html",i="children",j="主页面",k="主页面.html",l="信息展示",m="信息展示.html",n="郑韩故城概要",o="郑韩故城概要.html",p="郑韩故城地图",q="郑韩故城地图.html",r="遗址航拍影像",s="遗址航拍影像.html",t="专项评估",u="专项评估.html",v="辅助规划",w="辅助规划.html",x="成果管理",y="成果管理.html",z="综合检索",A="综合检索.html",B="统计分析",C="统计分析.html",D="系统管理",E="系统管理.html";
-return _creator();
-})();
